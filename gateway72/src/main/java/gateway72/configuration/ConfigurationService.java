d4fea618912d4ef0692aded502db0aa4168aebb6
Marcus Warm <warm@mwvb.de>
2019-06-16 12:59:43 +0200

    better configuration dump

--- gateway72/src/main/java/gateway72/configuration/ConfigurationService.java
+++ gateway72/src/main/java/gateway72/configuration/ConfigurationService.java
@@ -1,34 +1,74 @@
 package gateway72.configuration;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import gateway72.Gateway72Logger;
 
 public class ConfigurationService {
 
     public static void dump(Configuration model) {
-        List<String> urls = new ArrayList<>();
-        // TODO Verben zusammenfassen
+        List<RouteWithVerbs> routes = new ArrayList<>();
         for (RouteDefinition route : model.getRoutes()) {
-            urls.add(route.getUrl() + "  " + route.getVerb().name());
+            String verb = route.getVerb().name();
+            boolean found = false;
+            for (RouteWithVerbs r : routes) {
+                if (r.getUrl().equals(route.getUrl())) {
+                    r.addVerb(verb);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                routes.add(new RouteWithVerbs(route.getUrl(), verb));
+            }
         }
-        Collections.sort(urls);
+        routes.sort((a, b) -> a.getUrl().compareTo(b.getUrl()));
         Gateway72Logger.instance.dump("All routes:");
-        for (String url : urls) {
-            Gateway72Logger.instance.dump(" - " + url);
+        for (RouteWithVerbs r : routes) {
+            Gateway72Logger.instance.dump(" - " + r);
         }
         
-        List<String> services = new ArrayList<>();
-        for (Service service : model.getServices()) {
-            services.add(service.getName() + " -> " + service.getUrl());
+        if (!model.getServices().isEmpty()) {
+            List<Service> services = new ArrayList<>(model.getServices());
+            services.sort((a, b) -> a.getName().compareTo(b.getName()));
+            int max = services.stream().map(s -> s.getName().length()).max(Integer::compareTo).orElse(0);
+            Gateway72Logger.instance.dump("All services:");
+            for (Service service : services) {
+                Gateway72Logger.instance.dump(" - " + pad(service.getName(), max) + " -> " + service.getUrl());
+            }
         }
-        Collections.sort(services);
-        Gateway72Logger.instance.dump("All services:");
-        // TODO output as table
-        for (String name : services) {
-            Gateway72Logger.instance.dump(" - " + name);
+    }
+    
+    private static String pad(String text, int len) {
+        while (text.length() < len) {
+            text += " ";
+        }
+        return text;
+    }
+    
+    private static class RouteWithVerbs {
+        private String url;
+        private final Set<String> verbs = new TreeSet<>();
+
+        public RouteWithVerbs(String url, String verb) {
+            this.url = url;
+            verbs.add(verb);
+        }
+
+        public String getUrl() {
+            return url;
+        }
+        
+        public void addVerb(String verb) {
+            verbs.add(verb);
+        }
+
+        public String toString() {
+            return url + "  " + verbs.stream().collect(Collectors.joining(", "));
         }
     }
 }
